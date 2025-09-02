29d267404cb9ec1a2c16ffacd2b07c468a8fba61
Lucas Domingues <lucasrdomingues-lrd@hotmail.com>
2025-09-02 12:14:28 -0300

    Updated error messages

--- src/controllers/FileController.py
+++ src/controllers/FileController.py
@@ -1,68 +1,64 @@
 from src.validators import FileValidator
 from src.services import YoutubeService, TextService, PdfService, AudioService, VideoService
 from src.enums import HttpEnum
+from src.exceptions import AppException
 
 def analyze_youtube(url):
     service = YoutubeService(url)
     text = service.transcribe()
     
-    code = HttpEnum.Code.OK
-    message = ''
-
     if not text:
-        code = HttpEnum.Code.INTERNAL_SERVER_ERROR
-        message = HttpEnum.Message.INTERNAL_SERVER_ERROR
+        raise AppException(
+            code = HttpEnum.Code.INTERNAL_SERVER_ERROR,
+            message = HttpEnum.Message.INTERNAL_SERVER_ERROR
+        )
 
-    return text, code, message
+    return text, HttpEnum.Code.OK, ''
 
 async def analyze_video(file):
     service = VideoService(file)
     text = await service.transcribe()
     
-    code = HttpEnum.Code.OK
-    message = ''
-
     if not text:
-        code = HttpEnum.Code.INTERNAL_SERVER_ERROR
-        message = HttpEnum.Message.INTERNAL_SERVER_ERROR
+        raise AppException(
+            code = HttpEnum.Code.INTERNAL_SERVER_ERROR,
+            message = HttpEnum.Message.INTERNAL_SERVER_ERROR
+        )
 
-    return text, code, message
+    return text, HttpEnum.Code.OK, ''
 
 async def analyze_text(file):
     service = TextService(file)
     text = await service.transcribe()
     
-    code = HttpEnum.Code.OK
-    message = ''
-
     if not text:
-        code = HttpEnum.Code.INTERNAL_SERVER_ERROR
-        message = HttpEnum.Message.INTERNAL_SERVER_ERROR
+        raise AppException(
+            code = HttpEnum.Code.INTERNAL_SERVER_ERROR,
+            message = HttpEnum.Message.INTERNAL_SERVER_ERROR
+        )
 
-    return text, code, message
+    return text, HttpEnum.Code.OK, ''
 
 async def analyze_pdf(file):
     service = PdfService(file)
     text = await service.transcribe()
     
-    code = HttpEnum.Code.OK
-    message = ''
-
     if not text:
-        code = HttpEnum.Code.INTERNAL_SERVER_ERROR
-        message = HttpEnum.Message.INTERNAL_SERVER_ERROR
+        raise AppException(
+            code = HttpEnum.Code.INTERNAL_SERVER_ERROR,
+            message = HttpEnum.Message.INTERNAL_SERVER_ERROR
+        )
 
-    return text, code, message
+    return text, HttpEnum.Code.OK, ''
 
 async def analyze_audio(file):
     service = AudioService(file)
     text = await service.transcribe()
     
-    code = HttpEnum.Code.OK
-    message = ''
-
     if not text:
-        code = HttpEnum.Code.INTERNAL_SERVER_ERROR
-        message = HttpEnum.Message.INTERNAL_SERVER_ERROR
+        raise AppException(
+            code = HttpEnum.Code.INTERNAL_SERVER_ERROR,
+            message = HttpEnum.Message.INTERNAL_SERVER_ERROR
+        )
 
-    return text, code, message
+    return text, HttpEnum.Code.OK, ''
